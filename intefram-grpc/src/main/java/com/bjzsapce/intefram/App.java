482ab13a14f3618f09dc9ca747fa691662db8cf9
liuwenqing <610335123@qq.com>
2024-11-06 09:32:45 +0800

    first commit

---
+++ intefram-grpc/src/main/java/com/bjzsapce/intefram/App.java
@@ -0,0 +1,13 @@
+package com.bjzsapce.intefram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
